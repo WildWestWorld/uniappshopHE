7e9039ecaf294a24374bb92c92bf8c063ee76d4b
JOTARO <380686356@qq.com>
2021-11-05 23:29:40 +0800

    1.1Updata

---
+++ components/goods-card/goods-card.vue
@@ -0,0 +1,56 @@
+<template>
+	<navigator class="goods-item">
+	
+		<u-image class="u-skeleton-fillet" width="100%" height="300rpx" :src="item.cover_url"></u-image>
+	
+		<!-- 商品名字 -->
+		<view class="title u-line-1 u-skeleton-rect" >{{item.title?item.title:'商品名称'}}</view>
+	
+		<!-- 价格和销量 -->
+		<view class="u-flex u-row-between ">
+			<view class="price u-skeleton-rect">价格：￥{{item.price}}</view>
+			<view class="sale u-skeleton-rect"> 销量：{{item.sales}}</view>
+		</view>
+	</navigator>
+</template>
+
+<script>
+	export default {
+		name:"goods-card",
+		props:{
+			item:{
+				type:Object,
+				default(){
+					return{}
+				}
+			}
+		},
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.goods-item {
+		margin-top: 30rpx;
+		padding: 40rpx;
+		box-shadow: 0 12rpx 20rpx 0 rgba(0, 0, 0, 0.1)
+	}
+
+	.title {
+		margin-top: 10rpx;
+		font-weight: 1000;
+		font-size: 32rpx;
+	}
+
+	.price {
+		color: red;
+	}
+
+	.sale {
+		color: #888;
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,30 +1,21 @@
 <template>
-	<view >
-	
-	  <u-swiper :list="imgList"></u-swiper>
-	  
-	  <view class="u-m-t-15">
-	  <u-tabs :list="sortList" bar-width=100 :is-scroll="false" :current="currentSort" @change="changeSort"></u-tabs>
-	</view>
-					<u-row gutter="16">
-						<u-col span="6"v-for="i in 6">
-							
-								<navigator class="goods-item">
-									<u-image width="100%" height="300rpx" src="https://oss.shop.eduwork.cn/product/2020-0820-5f3e092c2d07f.png"></u-image>
-								
-						
-							<!-- 商品名字 -->
-							<view class="title">javascript</view>
-							
-							<!-- 价格和销量 -->
-							<view class="u-flex u-row-between">
-								<view class="price">价格：￥50</view>
-								<view class="sale">	销量：10</view>
-							</view>
-							</navigator>
-							
-						</u-col>
-					</u-row>
+	<view class="u-skeleton">
+
+		<u-swiper :list="slides" name="img_url" height="320"></u-swiper>
+
+		<view class="u-m-t-15">
+			<u-tabs :list="sortList" bar-width=100 :is-scroll="false" :current="currentSort" @change="changeSort">
+			</u-tabs>
+		</view>
+		<u-row gutter="16">
+			<u-col span="6" v-for="item in goods.length !=0 ?goods : [{},{},{},{},{}]">
+
+				<goods-card :item="item"></goods-card>
+
+			</u-col>
+		</u-row>
+		<!--骨架屏-->
+		<u-skeleton :loading="loading" :animation="true" bgColor="#FFF"></u-skeleton>
 	</view>
 </template>
 
@@ -32,47 +23,61 @@
 	export default {
 		data() {
 			return {
-			imgList:[
-				'https://cdn.uviewui.com/uview/swiper/3.jpg',
-				'https://cdn.uviewui.com/uview/swiper/2.jpg'
-			],
-			sortList:[
-				{name:'默认'},
-				{name:'销量'},
-				{name:'推荐'},
-				{name:'最新'},
-			],
-			currentSort: 0,
+
+				sortList: [{
+						name: '默认'
+					},
+					{
+						name: '销量'
+					},
+					{
+						name: '推荐'
+					},
+					{
+						name: '最新'
+					},
+				],
+				currentSort: 0,
+				slides: [],
+				goods: [],
+				page: 1,
+				loading:false
 			}
 		},
 		async onLoad() {
-		this.$u.api.index().then(res=>{
-			console.log(res)
-		})
+			this.getData()
 		},
+
 		methods: {
 			changeSort(index) {
 				this.currentSort = index
-		}
+				this.getData()
+				//点击就重置分页和商品数据
+				this.goods = [];
+				this.page = 1;
+				this.getData();
+			},
+			async getData() {
+				this.loading= true //显示骨架屏
+				const params = {
+					page: this.page
+				}
+				if (this.currentSort == 1) params.sales = 1;
+				if (this.currentSort == 2) params.recommand = 1;
+				if (this.currentSort == 3) params.new = 1;
+				const res = await this.$u.api.index(params)
+				this.loading= false //隐藏骨架屏
+				this.slides = res.slides
+				this.goods = [...this.goods, ...res.goods.data]
+			},
+			onReachBottom() {
+				this.page += 1
+				this.getData()
+			}
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
-	.goods-item{
-		margin-top: 30rpx;
-		padding: 40rpx;
-		box-shadow:0 12rpx 20rpx 0 rgba(0,0,0,0.1)
-	}
-	.title{
-		margin-top:10rpx ;
-		font-weight:1000;
-		font-size:32rpx;
-	}
-	.price{
-		color:red;
-	}
-	.sale{
-		color:#888;
-	}
+
 </style>
